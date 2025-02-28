29200ebc4afafc8b361864f723df7954fe26c6d6
Kishore210905 <162281217+Kishore210905@users.noreply.github.com>
2025-02-28 14:08:05 +0530

    Update calc.java by feature2

--- calc.java
+++ calc.java
@@ -7,7 +7,7 @@ public class Calc {
     }
     public static void main(String[] args) {
         Calc calculator = new Calc();
-        System.out.println("Multiplication: " + calculator.mul(5, 3)); 
-        System.out.println("Division: " + calculator.div(6, 3));
+        System.out.println("Multiplication:"+ calculator.mul(5,3)); 
+        System.out.println("Division:"+ calculator.div(6,3));
     }
 }
